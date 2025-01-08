0e1cc6ae278dac87501fe7f44fa2f2ba5a3c4164
elmokaber <elmokaber@MAWADA>
2025-01-08 18:57:16 +0200

    Add project files.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ConsoleApp4
+{
+    
+    public static class Extension
+    {
+        
+
+        public static string getName(this string name,string text)
+        {
+            return text +" "+ name;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Hala";
+            Console.WriteLine(name.getName("Hello"));
+        }
+    }
+    
+}
